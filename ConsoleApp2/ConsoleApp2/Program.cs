e1eceb2982366f4c1485dd5d45010f1b4cde9650
Maciej <petrykowski99@gmail.com>
2020-03-10 14:19:38 +0100

    2

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //
+            var inFile = args.Length > 0 ? args[0] : "data.csv";
+            var outFile = args.Length > 1 ? args[1] : "result.xml";
+            var type = args.Length > 2 ? args[2] : "xml";
+            //
+            List<Student> students= Read.readStudents(inFile);
+            //
+            if (type == "xml") 
+                Write.writeToXML(outFile,students);
+        }        
+    }
+}
